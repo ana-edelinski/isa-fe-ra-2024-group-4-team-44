9c989e78dff66e42604e2a4045eef2b6f4dab662
Maša Mastilović <masamastilovic@gmail.com>
2024-11-09 19:26:41 +0100

    feat: adding new post

--- only-buns/src/app/create-post/create-post.component.ts
+++ only-buns/src/app/create-post/create-post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnDestroy, OnInit} from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,6 +11,9 @@ import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { Post } from '../model/post.model';
 import { PostService } from '../post.service';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../profile/user.model';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -27,18 +30,21 @@ import { PostService } from '../post.service';
   templateUrl: './create-post.component.html',
   styleUrl: './create-post.component.css'
 })
-export class CreatePostComponent {
+export class CreatePostComponent implements OnInit, OnDestroy {
 
-  constructor(private postService: PostService) {} 
+  constructor(private postService: PostService, private authService: AuthService, private router: Router) {} 
+
+  user: User = new User();
+  private userSubscription: Subscription = Subscription.EMPTY;
+
 
   fb = new FormBuilder()
   selectedFile: File | null = null;
   imagePath: string | null = null;
-  newPost: Post | null = null;
 
   postForm = new FormGroup({
     description: new FormControl('', [Validators.required]),
-    image: new FormControl('', [Validators.required]),
+    image: new FormControl(''),
     address: this.fb.group({
       city: ['', Validators.required],
       street: ['', Validators.required],
@@ -46,29 +52,106 @@ export class CreatePostComponent {
     })
   });
 
-  onImageSelect(event: any){
-    const file:File = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = () => {
-        this.imagePath = reader.result as string;
-        this.postForm.patchValue({
-          image: this.imagePath
+  ngOnInit(): void {
+    this.resetImagePath();
+    this.postForm.reset()
+    this.userSubscription = this.authService.getUser().subscribe(user => {
+      if (user && user.id) {
+        this.user = user; 
+      } else {
+        this.router.navigate(['']);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+
+
+  resetImagePath() {
+    this.imagePath = null;
+    this.selectedFile = null;
+    this.postForm.patchValue({
+      image: null 
+    });
+    
+  }
+
+  
+
+  onImageSelect(event: any) {
+    
+    if (event.target.files[0]) {
+      this.selectedFile = event.target.files[0]; 
+    }
+  }
+
+  uploadImage() {
+    if (!this.selectedFile) {
+      alert("A photo is needed.")
+      return;
+    }  
+
+    this.postService.uploadImage(this.selectedFile).subscribe( {
+      next: (result: any) => {
+        console.log(result);
+        this.imagePath = result.imagePath;
+        this.onPostSubmit()
+      },
+      error: () => {
+        alert('An error has occured uploading the photo.');
+      }
+    })
+  }
+
+
+  onPostSubmit() {
+    if (this.postForm.valid) {
+      console.log(this.imagePath)
+      if(this.imagePath !== null && this.user.id != null){
+        const newPost: Post = {
+          id: 100,
+          creatorId: this.user.id,
+          creatorUsername: '',
+          description: this.postForm.value.description || '',
+          creationTime: new Date(),
+          imagePath: this.imagePath, // Dodeljujemo putanju koju smo dobili sa servera
+          locationStreet: this.postForm.value.address?.street || '',
+          locationCity: this.postForm.value.address?.city || '',
+          locationPostalCode: this.postForm.value.address?.postalCode || '',
+          comments: [],
+          likes: []
+        };
+
+        this.postService.createPost(newPost).subscribe({
+          next: (result: Post) => {
+            if (result) {
+              this.resetImagePath()
+              console.log('Post created successfully');
+              this.router.navigate(['']);
+            } else {
+              alert('An error has occurred. Please try again.');
+            }
+          },
+          error: () => {
+            alert('An error has occurred. Please try again.');
+          }
         });
-    };
-    reader.readAsDataURL(file); 
+      } else {
+        console.log("Error uploading photo")
+      }   
+    } else {
+      console.log("Form not valid")
+    }
+  }
+
 }
 
 
 
   
 
-  onPostSubmit() {
-    if (this.postForm.valid) {
-      console.log(this.postForm.value);
-      
-    } else {
-      console.log('Form is invalid');
-    }
-  }
-
-}
+ 
